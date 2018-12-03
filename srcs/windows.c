48f1518498dee4d31e0582e78d60fa372f4d25d8
hbally <hbally@student.42.fr>
2018-12-03 19:41:58 +0100

    windows.c changed

--- srcs/windows.c
+++ srcs/windows.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 18:17:23 by hbally            #+#    #+#             */
-/*   Updated: 2018/12/03 19:35:56 by hbally           ###   ########.fr       */
+/*   Updated: 2018/12/03 19:41:37 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,25 +56,17 @@ static void		create_window(t_hub *hub)
 
 int			key_hook(int keycode, void *param)
 {
-	ft_putstr("test1");
-	*(int*)(param) = 5;
-	ft_putstr("test2");
+	param = 0;
 	keycode = 10;
-	return (1);
+	return (0);
 }
 
 void			start_window(t_hub *hub)
 {
-	int a;
-
-	a = 0;
 	create_window(hub);
-	ft_putstr("test3");
 	if (hub->win->mlx_id && hub->win->self_id)
 	{
-		ft_putstr("test4");
-		mlx_key_hook(hub->win->self_id, &key_hook, &a);
-		ft_putstr("test5");
+		//mlx_key_hook(hub->win->self_id, &key_hook, hub);
 		mlx_loop(hub->win->mlx_id);
 	}
 }
